349d43b2f9f8b419edd80ca7077174e4fa537151
ryantaylor16 <ryan.taylor0@gmail.com>
2016-11-03 08:58:18 -0700

    Update settings.py

--- sendyrsspub-master/settings.py
+++ sendyrsspub-master/settings.py
@@ -1,12 +1,12 @@
 DEFAULTS = {
     'sendy_url': 'https://www.allteamz.com/sendy/',
-    'sendy_api_key': 'DNdFCsot8ueSfC4XWusY',
+    'sendy_api_key': '$APIKEY',
     'database': 'feed_nlog.db',
     'feed_url': 'http://www.allteamz.com/blog/feed/',
     'template': 'rss-feed_broken.html,test.txt',
     'from_name': 'AllTeamz',
     'from_email': 'talent@allteamz.com',
     'reply_to': 'talent@allteamz.com',
-    'subject': 'Do you like fundraising for youth sports? NO!',
-    'list_ids': 'Hin05mr1sujTV8l2x892AFPA'
+    'subject': 'SUBJECT UPDATE',
+    'list_ids': '$LISTID'
 }
