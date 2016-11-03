5aa8f1533cc474652e977ebcda39d07dcbcdf309
Ryan Taylor <ryan_taylor@intuit.com>
2016-11-03 08:56:52 -0700

    Working with RSS and Sendy

---
+++ sendyrsspub-master/settings.py
@@ -0,0 +1,12 @@
+DEFAULTS = {
+    'sendy_url': 'https://www.allteamz.com/sendy/',
+    'sendy_api_key': 'DNdFCsot8ueSfC4XWusY',
+    'database': 'feed_nlog.db',
+    'feed_url': 'http://www.allteamz.com/blog/feed/',
+    'template': 'rss-feed_broken.html,test.txt',
+    'from_name': 'AllTeamz',
+    'from_email': 'talent@allteamz.com',
+    'reply_to': 'talent@allteamz.com',
+    'subject': 'Do you like fundraising for youth sports? NO!',
+    'list_ids': 'Hin05mr1sujTV8l2x892AFPA'
+}
